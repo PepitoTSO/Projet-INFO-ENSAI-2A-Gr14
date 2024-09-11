81be589ebc93edb340daace0cd08c1a6ac433cde
Mirlon382 <139069943+Mirlon382@users.noreply.github.com>
2024-09-11 16:23:50 +0200

    update api

--- src/Api_FreeSound/apifreesound.py
+++ src/Api_FreeSound/apifreesound.py
@@ -1,36 +1,34 @@
 import os
 import requests
 import dotenv
+import json
 
 class apifreesound():
     '''Gestion API'''
 
-    def requestget(self, recherche: str, params=False, OAuth2=False):
+    def requestget(recherche: str, params=False, OAuth2=False):
         '''
         Get http avec la clef API selon recherche
 
         Params:
             recherche : str
             Ce que l'on recherche sur freesound
-
             params : bool
             Si besoin de filtres
-
             OAuth2 : bool
             Si authentification en OAuth2
-
         Returns:
             reponse : json
             ce que renvoie l'api en json
         '''
-        dotenv.load_dotenv()
+
         # Verifie puis recupere la cle API dans le .env
+        dotenv.load_dotenv()
         try:
-            os.environ['CLEAPI']
+            cleAPI = os.getenv('CLEAPI')
         except KeyError:
             print("Manque la variable d'environnement CLEAPI")
 
-        cleAPI = os.environ['CLEAPI']
         payload = {'query': recherche, 'token': cleAPI}
 
         if params:
@@ -48,13 +46,17 @@ class apifreesound():
 
         reponse.raise_for_status()
 
-        return reponse.json()
+        repjson = reponse.json()
 
-    def parserjson(self, json):
+        return repjson
+
+    def parserjson(repjson) -> list:
         '''
         adapte le json retourné par requestget à la base de données
 
-        reponse type
+        Parameters :
+
+        repjson : json
         {
         "count": 2887, "previous": null, "next": "https://freesound.org/apiv2/search/text/?&query=test&weights=&page=2", "results": 
         [
@@ -69,12 +71,43 @@ class apifreesound():
         {"id": 64363, "name": "test_elecdict.wav", "tags": ["doodling", "guitar", "test", "usb"], "license": "http://creativecommons.org/licenses/by/3.0/", "username": "NoiseCollector"},
         {"id": 64364, "name": "test_elecdict_vol1.wav", "tags": ["doodling", "guitar", "test", "usb"], "license": "http://creativecommons.org/licenses/by/3.0/", "username": "NoiseCollector"}, {"id": 64365, "name": "test_electure.wav", "tags": ["doodling", "guitar", "test", "usb"], "license": "http://creativecommons.org/licenses/by/3.0/", "username": "NoiseCollector"}, {"id": 64367, "name": "test_guitar2.wav", "tags": ["acoustic", "doodling", "guitar", "microphone", "test", "usb"], "license": "http://creativecommons.org/licenses/by/3.0/", "username": "NoiseCollector"}, {"id": 165277, "name": "Surround Test - Channel Names", "tags": ["multichannel", "name", "calibration", "track", "multi-channel", "surround", "calibrate", "test", "naming"], "license": "https://creativecommons.org/licenses/by/4.0/", "username": "blouhond"}, {"id": 436839, "name": "test.wav", "tags": ["test", "sound", "soundtest"], "license": "http://creativecommons.org/publicdomain/zero/1.0/", "username": "thedumbstudio"}
         ]
-        }     
+        }
+
+        Returns :
+
+        repjson : list
+        [
+        {
+            "id": 326361,
+            "name": "ebs test3.wav",
+            "tags": [
+            "test",
+            "home",
+            "bass"
+            ],
+            "license": "http://creativecommons.org/licenses/by-nc/3.0/",
+            "username": "wazdabaz"
+        },
+        {
+            "id": 326360,
+            "name": "ebs test4.wav",
+            "tags": [
+            "test",
+            "home",
+            "bass"
+            ],
+            "license": "http://creativecommons.org/licenses/by-nc/3.0/",
+            "username": "wazdabaz"
+        },
 
         '''
-        # Peut etre une methode abstraite
-        # a adapter pour la DAO, méthode ajout de données
-        pass
+
+        repjson = repjson['results']
+
+        repjson = json.dumps(repjson, indent=2)
+
+        return repjson
+
 
     def apiOAuth2(self, recherche):
         '''
@@ -85,9 +118,29 @@ class apifreesound():
         Step 3: Your application uses that authorization grant to request an access token that ‘links’ the end user with your application and that you will then need to add to all your API requests.
 
         '''
-        # A voir si nécessaire
-        pass
+        #Step1
+        client_id =
+        r_type = 
 
+        getpayload = {'client_id' = client_id, 'response_type' = r_type}
+
+        requests.get('https://freesound.org/apiv2/oauth2/authorize/', getpayload)
+
+        #step2
+        #il faut autoriser l'appli et rentrer le code.
+        code_auth = input("Le code donné")
+
+        postpayload = {"client_id" = client_id, "client_secret" = client_secret, 'grant_type' = authorization_code, "code" = code_auth}
+        
+        rep = requests.post("https://freesound.org/apiv2/oauth2/access_token/", postpayload)
+        
+        rep.raise_for_status()
+
+        rep["access_token"]
+        #le token est valable 24h après il faut le refresh a partir du refresh_token dans la réponse
+        return
     def filtre(self, params):
-        '''permet de construire le payload http pour filtrer la requete de requestget'''
+        '''
+        Permet de construire le payload http pour filtrer la requete de requestget
+        '''
         pass
