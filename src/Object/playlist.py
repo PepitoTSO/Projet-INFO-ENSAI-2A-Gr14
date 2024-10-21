9f93a7f195078b3ca5bf4b3b569bc20e637cd4b8
ArmandVi <armand.vincent77@gmail.com>
2024-10-21 16:19:55 +0200

    Update playlist

--- src/Object/playlist.py
+++ src/Object/playlist.py
@@ -3,6 +3,7 @@ from src.Object.utilisateur import Utilisateur
 class Playlist:
 
     def __init__(self, utilisateur : Utilisateur, id_playlist : int, nom_playlist, dict_son):
+
         if not isinstance(utilisateur, Utilisateur):
             raise TypeError("L'utilisateur n'est pas de la classe utilisateur.")
         if not isinstance(id_playlist, int):
@@ -11,6 +12,7 @@ class Playlist:
             raise TypeError("Le nom de la playlist doit être un str")
         if not isinstance(dict_son, list):
             raise TypeError("La liste de son doit être une liste.")
+
         self.utilisateur = utilisateur
         self.id_playlist = id_playlist
         self.nom_playlist = nom_playlist
@@ -43,23 +45,33 @@ class Playlist:
 
         service = PlaylistService(self).retirer_son_de_playlist(son : Son)
 
-    def changer_ordre(self, son, ajout : bool = True):
-        if bool:
-            for i in range(len(self.dict_son)):
-                if self.dict_son[i][1] > ordre:
-                    self.dict_son[i][1] += 1
-        else:
-            for i in range(len(self.dict_son)):
-                if self.dict_son[i][1] > son:
-                    self.dict_son[i][1] += -1
+    def changer_ordre(self, son, ordre : int):
 
         for i in range(len[self.dict_son]):
             if self.dict_son[i][0] == son:
+                ordre_ancien = self.dict_son[i][1]
                 self.dict_son.pop(i)
+
+        for i in range(len[self.dict_son]):
+            if self.dict_son[i][1] > ordre_ancien:
+                self.dict_son[i][1] += -1
+
+        for i in range(len[self.dict_son]):
+            if self.dict_son[i][1] >= ordre:
+                self.dict_son[i][1] += 1
+
         self.dict_son.append([son, ordre])
 
-        ## à revoir
+        PlaylistService(self).changer_ordre_son(son, ordre)
+
+    def changer_nom_playlist(self, nouveau_nom):
+        if not isinstance(nouveau_nom, str):
+            raise TypeError("Le nouveau nom doit être un str.")
+
+        self.nom_playlist = nouveau_nom
+
+        PlaylistService(self).changer_nom_playlist(nouveau_nom)
 
 
-    def
+
 
