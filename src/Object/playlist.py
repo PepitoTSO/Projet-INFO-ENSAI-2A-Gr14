81178f5fc64031c34500d122b339cc967a7e3931
ArmandVi <armand.vincent77@gmail.com>
2024-10-21 16:55:31 +0200

    Last commit of the day

--- src/Object/playlist.py
+++ src/Object/playlist.py
@@ -7,7 +7,11 @@ class Playlist:
     """Voici la classe qui gère les playlist"""
 
     def __init__(
-        self, utilisateur: Utilisateur, id_playlist: int, nom_playlist, dict_son
+        self,
+        utilisateur: Utilisateur,
+        id_playlist: int,
+        nom_playlist: str,
+        list_son: list,
     ):
 
         if not isinstance(utilisateur, Utilisateur):
@@ -16,13 +20,13 @@ class Playlist:
             raise TypeError("L'id de la playlist doit être de type int.")
         if not isinstance(nom_playlist, str):
             raise TypeError("Le nom de la playlist doit être un str")
-        if not isinstance(dict_son, list):
+        if not isinstance(list_son, list):
             raise TypeError("La liste de son doit être une liste.")
 
         self.utilisateur = utilisateur
         self.id_playlist = id_playlist
         self.nom_playlist = nom_playlist
-        self.dict_son = dict_son
+        self.list_son = list_son
 
     def ajouter_son(self, son: Son, ordre: int):
         if not isinstance(ordre, int):
@@ -30,44 +34,46 @@ class Playlist:
         if not isinstance(son, Son):
             raise TypeError("son doit être un objet de classe Son.")
 
-        for i in range(len(self.dict_son)):
-            if self.dict_son[i][1] > ordre:
-                self.dict_son[i][1] += 1
+        for i in range(len(self.list_son)):
+            if self.list_son[i][1] > ordre:
+                self.list_son[i][1] += 1
 
-        self.dict_son.append([son, ordre])
+        self.list_son.append([son, ordre])
 
         PlaylistService(self).ajouter_son_a_playlist(son)
 
     def supprimer_son(self, son: Son):
-        for i in range(len(self.dict_son)):
-            if self.dict_son[i][0] == son:
-                ordre = self.dict_son[i][1]
-                self.dict_son.pop(i)
-        for i in range(len(self.dict_son)):
-            if self.dict_son[i][1] > ordre:
-                self.dict_son[i][1] += -1
+        for i in range(len(self.list_son)):
+            if self.list_son[i][0] == son:
+                ordre = self.list_son[i][1]
+                self.list_son.pop(i)
+        for i in range(len(self.list_son)):
+            if self.list_son[i][1] > ordre:
+                self.list_son[i][1] += -1
 
         PlaylistService(self).retirer_son_de_playlist(son)
 
     def changer_ordre(self, son, ordre: int):
 
-        if ordre > len[self.dict_son] + 1 or ordre < 0:
-            raise ValueError("La position ne peut etre inférieure à 0 ou supérieure à n + 1")
+        if ordre > len[self.list_son] + 1 or ordre < 0:
+            raise ValueError(
+                "La position ne peut etre inférieure à 0 ou supérieure à n + 1"
+            )
 
-        for i in range(len[self.dict_son]):
-            if self.dict_son[i][0] == son:
-                ordre_ancien = self.dict_son[i][1]
-                self.dict_son.pop(i)
+        for i in range(len[self.list_son]):
+            if self.list_son[i][0] == son:
+                ordre_ancien = self.list_son[i][1]
+                self.list_son.pop(i)
 
-        for i in range(len[self.dict_son]):
-            if self.dict_son[i][1] > ordre_ancien:
-                self.dict_son[i][1] += -1
+        for i in range(len[self.list_son]):
+            if self.list_son[i][1] > ordre_ancien:
+                self.list_son[i][1] += -1
 
-        for i in range(len[self.dict_son]):
-            if self.dict_son[i][1] >= ordre:
-                self.dict_son[i][1] += 1
+        for i in range(len[self.list_son]):
+            if self.list_son[i][1] >= ordre:
+                self.list_son[i][1] += 1
 
-        self.dict_son.append([son, ordre])
+        self.list_son.append([son, ordre])
 
         PlaylistService(self).changer_ordre_son(son, ordre)
 
--- src/Service/playlist_service.py
+++ src/Service/playlist_service.py
@@ -5,9 +5,6 @@ class PlaylistService:
 
     def __init__(self, playlist: Playlist):
         if not isinstance(playlist, Playlist):
-            raise TypeError("La plylist n'est pas type Playlist.")
+            raise TypeError("La playlist n'est pas type Playlist.")
 
         self.playlist = playlist
-
-    
-
