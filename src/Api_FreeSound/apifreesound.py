a38fe68e11e663eaeecb6452a4960a50db766424
Mirlon382 <mirlon382@gmail.com>
2024-10-22 20:36:46 +0200

    view quasi fonctionnel

--- src/Api_FreeSound/apifreesound.py
+++ src/Api_FreeSound/apifreesound.py
@@ -56,30 +56,33 @@ class apifreesound():
 
         fichier = Path(f"../data/son/{id}.mp3")
 
-        if not fichier.exists():       
-            payload = {'token': self.cleAPI}
-            reponse = requests.get(
-                f'{self.url}/apiv2/sounds/{id}/',
-                params=payload,
-                timeout=1
-                )
+        if not fichier.exists():
+            try:     
+                payload = {'token': self.cleAPI}
+                reponse = requests.get(
+                    f'{self.url}/apiv2/sounds/{id}/',
+                    params=payload,
+                    timeout=1
+                    )
 
-            if HQ:
-                url_dl = reponse.json()['previews']['preview-hq-mp3']
-            else:
-                url_dl = reponse.json()['previews']['preview-lq-mp3']
+                if HQ:
+                    url_dl = reponse.json()['previews']['preview-hq-mp3']
+                else:
+                    url_dl = reponse.json()['previews']['preview-lq-mp3']
 
-            # Téléchargement du fichier
-            dl_path = gestion_dl.dossier
-            
-            reponse = requests.get(url_dl, stream=True)
+                # Téléchargement du fichier
+                dl_path = gestion_dl.dossier
+                
+                reponse = requests.get(url_dl, stream=True)
 
-            # Écriture du fichier dans le répertoire de destination
-            with open(f'{dl_path}/{id}.mp3', 'wb') as f:
-                for chunk in reponse.iter_content(chunk_size=8192):
-                    f.write(chunk)
+                # Écriture du fichier dans le répertoire de destination
+                with open(f'{dl_path}/{id}.mp3', 'wb') as f:
+                    for chunk in reponse.iter_content(chunk_size=8192):
+                        f.write(chunk)
 
-            print(f"Fichier téléchargé avec succès dans {dl_path}")
+                print(f"Fichier téléchargé avec succès dans {dl_path}")
+            except Exception as e:
+                print(f"Erreur lors du téléchargement du son avec ID {sound_id}: {e}")
         else :
             print("Le fichier existe dans data/son")
 
