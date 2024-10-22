2022118a9d792861ce484852e3601a5b52f3bcd6
mirlon <mirlon@localhost.localdomain>
2024-10-22 08:33:58 -0400

    debug

--- src/Object/son.py
+++ src/Object/son.py
@@ -1,37 +1,45 @@
+import pygame
+import sys
 from pathlib import Path
-import nava
+# Initialiser Pygame
+pygame.init()
+pygame.mixer.init()
+
+
 
 class Son():
     '''
-    Classe qui contient la définition d'un son : ses caractéristiques
+    Classe qui contient definition d'un son : ses caractéristiques
+
     '''
-    def __init__(self, id_son, nom, caracteristiques, path=None) -> None:
+    def __init__(self,id_son, nom, caracteristiques, path = None) -> None:
         self.id_son = id_son
         self.nom = nom
         self.caracteristiques = caracteristiques
         self.path_stockage = Path(path)
-        self.musique = nava.Audio(self.path_stockage)
+        self.musique = pygame.mixer.Sound(self.path_stockage)
 
     def __repr__(self):
         return f"{self.id_son=},{self.nom=},{self.caracteristiques=}"
 
     ## a relier à l'interface graphique
     def play(self):
+        
         self.musique.play()
 
     def pause(self):
-        self.musique.pause()
+        pygame.mixer.pause()
 
     def unpause(self):
-        self.musique.resume()
+        pygame.mixer.unpause()
 
     def stop(self):
         self.musique.stop()
 
     def jouer_en_boucle(self, temps):
-        self.musique.loop(temps)
+        pass
+
 
 if __name__ == "__main__":
     test=Son(1,'test','oui','data/test.mp3')
-    repr(test)
     test.play()
