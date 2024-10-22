98bdb8082399f368d0f05b9c3ff88959f988608b
ArmandVi <armand.vincent77@gmail.com>
2024-10-22 20:40:49 +0200

    Try hard de playlist_DAO

--- src/DAO/playlist_DAO.py
+++ src/DAO/playlist_DAO.py
@@ -5,6 +5,7 @@ from Object.playlist import Playlist
 from src.View.session import Session
 from src.DAO.utilisateur_DAO import Utilisateur_DAO
 from src.DAO.son_DAO import Son_DAO
+from src.Object.son import Son
 
 
 class Playlist_DAO(metaclass=Singleton):
@@ -13,18 +14,16 @@ class Playlist_DAO(metaclass=Singleton):
     Uses the Singleton pattern to ensure a single instance.
     """
 
-    def __init__(self):
-        self.utilisateur = Session().utilisateur
-
-
     def ajouter_playlist(self, nom_playlist) -> bool:
-        id_utilisateur = self.utilisateur.id
+        id_utilisateur = Session().utilisateur.id
 
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
-                cursor.execute(" INSERT INTO Playlist (id_utilisateur, nom)"
-                                "VALUES (%(id_utilisateur)s, %(nom_playlist)s);",
-                                {"id_utilisateur" : id_utilisateur, "nom_playlist" : nom_playlist})
+                cursor.execute(
+                    " INSERT INTO Playlist (id_utilisateur, nom)"
+                    "VALUES (%(id_utilisateur)s, %(nom_playlist)s);",
+                    {"id_utilisateur": id_utilisateur, "nom_playlist": nom_playlist},
+                )
             res = cursor.fetchone()
 
         if res:
@@ -32,13 +31,13 @@ class Playlist_DAO(metaclass=Singleton):
 
         return False
 
-
     def get_playlist_by_id(self, id_playlist: int):
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
-                    "SELECT * FROM playlists WHERE id_playlist = %(id)s",
-                    {"id": id_playlist})
+                    "SELECT * FROM playlist WHERE id_playlist = %(id)s",
+                    {"id": id_playlist},
+                )
 
                 res = cursor.fetchone()
         if res:
@@ -46,50 +45,68 @@ class Playlist_DAO(metaclass=Singleton):
             liste_son = Son_DAO().get_son_ordre_by_playlist(id_playlist)
 
             playlist = Playlist(
-                utilisateur =  user,
-                id_playlist= id_playlist,
-                nom_playlist= res["nom_playlist"],
-                list_son= liste_son,
-                )
+                utilisateur=user,
+                id_playlist=id_playlist,
+                nom_playlist=res["nom_playlist"],
+                list_son=liste_son,
+            )
         return playlist
 
-    def get_all_playlists(self) -> List:
-        try:
-            cursor = self.db_connection.cursor()
-            select_all_playlists_query = """
-                SELECT id_playlist, id_user, nom_playlist FROM playlists
-            """
-            cursor.execute(select_all_playlists_query)
-            playlists = []
-            for row in cursor.fetchall():
-                id_playlist = row["id_playlist"]
-                id_user = row["id_user"]
-                nom_playlist = row["nom_playlist"]
+    def get_all_playlists_by_user(self, id_user: int):
+        playlists = []
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM playlist WHERE id_utilisateur = %(id_user)s",
+                    {"id_user": id_user},
+                )
 
-                # Now get the songs for each playlist
-                select_songs_query = """
-                    SELECT song_name, song_order FROM playlist_songs
-                    WHERE id_playlist = %s ORDER BY song_order
-                """
-                cursor.execute(select_songs_query, (id_playlist,))
-                songs = [
-                    {"name": song["song_name"], "order": song["song_order"]}
-                    for song in cursor.fetchall()
-                ]
+                res = cursor.fetchall()
 
-                # Assuming you have the Playlist class imported
+        if res:
+            user = Utilisateur_DAO.get_utilisateur(self, id_user)
+            for playlist_data in res:
+                liste_son = Son_DAO().get_son_ordre_by_playlist(
+                    playlist_data["id_playlist"]
+                )
                 playlist = Playlist(
-                    id_user=id_user,
-                    id_playlist=id_playlist,
-                    nom_playlist=nom_playlist,
-                    dict_son=songs,
+                    utilisateur=user,
+                    id_playlist=playlist_data["id_playlist"],
+                    nom_playlist=playlist_data["nom_playlist"],
+                    list_son=liste_son,
                 )
                 playlists.append(playlist)
-            cursor.close()
-            return playlists
-        except Exception as e:
-            print(f"Error retrieving all playlists: {e}")
-            return []
+        return playlists
+
+    def supprimer_playlist(self, id_playlist):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "DELETE FROM son WHERE id_playlist = %(id_playlist)s",
+                    {"id_playlist": id_playlist},
+                )
+
+                cursor.execute(
+                    "DELETE FROM playlist WHERE id_playlist = %(id_playlist)s",
+                    {"id_playlist": id_playlist},
+                )
+
+            connection.commit()
+
+    def modifier_nom_playlist(self, id_playlist, nouveau_nom):
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "UPDATE playlists SET nom_playlist = %(nouveau_nom)s WHERE id_playlist = %(id_playlist)s",
+                    {"nouveau_nom": nouveau_nom, "id_playlist": id_playlist},
+                )
+            connection.commit()
+
+    # manque changer_ordre
+
+    def supprimer_son(self, id_playlist, son: Son):
+        id_son = son.id
+        Son_DAO().supprimer_son_by_playlist(id_playlist, id_son)
 
     def modifier_playlist(self, id_modif: Dict[str, Any]) -> bool:
         try:
