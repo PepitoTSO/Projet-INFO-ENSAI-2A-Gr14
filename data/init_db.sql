95fb5bd9c9ab53c767c9888bb75a7f33877eb7c1
Tom O'Connor <tom.o-connor@eleve.ensai.fr>
2024-10-22 16:11:00 +0200

    commit

--- data/init_db.sql
+++ data/init_db.sql
@@ -2,18 +2,17 @@ DROP SCHEMA IF EXISTS bdd CASCADE;
 CREATE SCHEMA bdd;
 
 --------------------------------------------------------------
--- Le compte
+-- Les utilisateurs
 --------------------------------------------------------------
 
-DROP TABLE IF EXISTS bdd.compte CASCADE ;
-CREATE TABLE bdd.compte (
+DROP TABLE IF EXISTS bdd.utilisateur CASCADE ;
+CREATE TABLE bdd.utilisateur (
     id_utilisateur integer PRIMARY KEY,
     pseudo varchar UNIQUE NOT NULL,
     mdp_hash varchar NOT NULL,
     date_creation date NOT NULL,
     date_derniere_co date NOT NULL
 );
-
 
 --------------------------------------------------------------
 -- Les playlists
@@ -27,20 +26,6 @@ CREATE TABLE bdd.playlist (
     nom varchar NOT NULL
 );
 
-
---------------------------------------------------------------
--- La table de jointure entre playlist et son
---------------------------------------------------------------
-
-DROP TABLE IF EXISTS bdd.playlist_son_join CASCADE ;
-
-CREATE TABLE bdd.playlist_son_join (
-    id_playlist integer FOREIGN KEY,
-    id_son integer UNIQUE NOT NULL,
-    ordre_son_in_plist integer NOT NULL
-);
-
-
 --------------------------------------------------------------
 -- Les sons
 --------------------------------------------------------------
@@ -49,7 +34,6 @@ DROP TABLE IF EXISTS bdd.son CASCADE;
 
 CREATE TABLE bdd.son (
     id_playlist integer PRIMARY KEY,
-    tags
+    tags varchar,
     path_stockage varchar NOT NULL
 );
-
