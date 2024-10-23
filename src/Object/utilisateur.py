235997c50fa9b298faff1950d8ca1bd62c410a25
HABIMANA Emmanuel <id2320@DOMENSAI.ECOLE>
2024-10-23 12:28:42 +0200

    id

--- src/Object/utilisateur.py
+++ src/Object/utilisateur.py
@@ -1,9 +1,6 @@
 class Utilisateur:
-    def __init__(self, id: int, dd, ddc, playlist):
+    def __init__(self, id: int):
         if not isinstance(id, int):
             raise TypeError("L'id de l'utilisateur n'est pas de la classe int.")
 
         self.id = id
-        self.date_debut = date_debut
-        self.date_derniere_co = date_derniere_co
-        self.playlist = playlist
