cc553aab712b62a07d70bbe110b3751d53fab6cd
Tom O'Connor <tom.o-connor@eleve.ensai.fr>
2024-10-24 16:42:24 +0200

    testplaylist

--- src/Object/utilisateur.py
+++ src/Object/utilisateur.py
@@ -1,6 +1,8 @@
 class Utilisateur:
-    def __init__(self, id: int):
+    def __init__(self, id_utilisateur: int, pseudo: str, mdp: str):
         if not isinstance(id, int):
             raise TypeError("L'id de l'utilisateur n'est pas de la classe int.")
 
-        self.id = id
+        self.id_utilisateur = id_utilisateur
+        self.pseudo = pseudo
+        self.mdp = mdp
--- src/Tests/test_objet1/test_playlist.py
+++ src/Tests/test_objet1/test_playlist.py
@@ -1,80 +1,78 @@
-import pytest
+import unittest
 from Object.utilisateur import Utilisateur
 from Object.son import Son
 from Object.playlist import Playlist
 
 
-@pytest.fixture
-def utilisateur():
-    return Utilisateur(1)
+def setUpModule():
+    print("Setting up module resources...")
 
 
-@pytest.fixture
-def son1():
-    return Son(1, "Song 1", "Caracteristiques 1")
+def tearDownModule():
+    print("Tearing down module resources...")
 
 
-@pytest.fixture
-def son2():
-    return Son(2, "Song 2", "Caracteristiques 2")
+class TestPlaylist(unittest.TestCase):
 
+    def setUp(self):
+        # Common setup for all tests
+        self.utilisateur = Utilisateur(1)
+        self.son1 = Son(1, "Song 1", "Caracteristiques 1")
+        self.son2 = Son(2, "Song 2", "Caracteristiques 2")
+        self.playlist = Playlist(self.utilisateur, 1, "Playlist 1", [])
 
-@pytest.fixture
-def playlist(utilisateur):
-    return Playlist(utilisateur, 1, "Playlist 1", [])
+    def tearDown(self):
+        # Cleanup (runs after every test)
+        self.playlist = None
 
+    def test_ajouter_son(self):
+        # GIVEN
+        son = self.son1
+        ordre = 1
 
-def test_ajouter_son(playlist, son1):
-    # GIVEN
-    son = son1
-    ordre = 1
+        # WHEN
+        self.playlist.ajouter_son(son, ordre)
 
-    # WHEN
-    playlist.ajouter_son(son, ordre)
+        # THEN
+        self.assertEqual(len(self.playlist.list_son), 1)
+        self.assertEqual(self.playlist.list_son[0][0], son)
+        self.assertEqual(self.playlist.list_son[0][1], ordre)
 
-    # THEN
-    assert len(playlist.list_son) == 1
-    assert playlist.list_son[0][0] == son
-    assert playlist.list_son[0][1] == ordre
+    def test_supprimer_son(self):
+        # GIVEN
+        son = self.son1
+        self.playlist.list_son = [[son, 1]]
 
+        # WHEN
+        self.playlist.supprimer_son(son)
 
-def test_supprimer_son(playlist, son1):
-    # GIVEN
-    son = son1
-    playlist.list_son = [[son, 1]]
+        # THEN
+        self.assertEqual(len(self.playlist.list_son), 0)
 
-    # WHEN
-    playlist.supprimer_son(son)
+    def test_changer_ordre(self):
+        # GIVEN
+        son = self.son1
+        ordre = 2
+        self.playlist.list_son = [[son, 1]]
 
-    # THEN
-    assert len(playlist.list_son) == 0
+        # WHEN
+        self.playlist.changer_ordre(son, ordre)
 
+        # THEN
+        self.assertEqual(len(self.playlist.list_son), 1)
+        self.assertEqual(self.playlist.list_son[0][0], son)
+        self.assertEqual(self.playlist.list_son[0][1], ordre)
 
-def test_changer_ordre(playlist, son1):
-    # GIVEN
-    son = son1
-    ordre = 2
-    playlist.list_son = [[son, 1]]
+    def test_changer_nom_playlist(self):
+        # GIVEN
+        nouveau_nom = "New Playlist Name"
 
-    # WHEN
-    playlist.changer_ordre(son, ordre)
+        # WHEN
+        self.playlist.changer_nom_playlist(nouveau_nom)
 
-    # THEN
-    assert len(playlist.list_son) == 1
-    assert playlist.list_son[0][0] == son
-    assert playlist.list_son[0][1] == ordre
-
-
-def test_changer_nom_playlist(playlist):
-    # GIVEN
-    nouveau_nom = "New Playlist Name"
-
-    # WHEN
-    playlist.changer_nom_playlist(nouveau_nom)
-
-    # THEN
-    assert playlist.nom_playlist == nouveau_nom
+        # THEN
+        self.assertEqual(self.playlist.nom_playlist, nouveau_nom)
 
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
