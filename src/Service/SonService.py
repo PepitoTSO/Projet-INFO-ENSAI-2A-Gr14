3ef580535081218c00f324589944be900c802798
mirlon <mirlon@localhost.localdomain>
2024-10-27 16:52:52 -0400

    update sonservice

--- src/Service/SonService.py
+++ src/Service/SonService.py
@@ -1,44 +1,53 @@
-'''
-Oui
-'''
+from Object.son import son
+from DAO.son_DAO import son_DAO
 from Object.son import Son
-#from DAO.son_DAO import SonDAO
 
-class SonService():
-    def __init__(self, son: Son):
-        if not isinstance(son, Son):
+
+class sonService:
+
+    def __init__(self, son: son):
+        if not isinstance(son, son):
             raise TypeError("La son n'est pas type son.")
 
         self.son = son
 
-    def ajouter_son(self, son, id_playlist=None, ordre=None):
-        try:
-            #SonDAO().ajouter_son(son, id_playlist, ordre)
-            return True
-        except Exception as e:
-            print(f"Prblm ajout son :{e}")
-            return False
-
-
     def supprimer_son(self):
-        #son_DAO().supprimer_son(self.son.id_son)
-        pass
+        son_DAO().supprimer_son(self.son.id_son)
 
-    def obtenir_son(self):
-        pass
+    ## la partie lecteur son
+    def play(self):
+        # Charger et jouer la musique
+        pygame.mixer.music.load(str(self.path_stockage))
+        pygame.mixer.music.play()
 
-    def lister_son(self):
-        pass
+        # Attendre que la musique soit terminée
+        while pygame.mixer.music.get_busy():
+            time.sleep(1)
 
-    def jouer_son_en_boucle(self):
-        pass
+    def pause(self):
+        pygame.mixer.music.pause()
 
-    def jouer_son(self):
-        print("lecture :",repr(self.son))
-        self.son.play()
-    
-    def changer_tags_son(self):
-        pass
+    def unpause(self):
+        pygame.mixer.music.unpause()
 
-    def superposer_son(self):
+    def stop(self):
+        pygame.mixer.music.stop()
+
+    def jouer_en_boucle(self, temps):
+        pygame.mixer.music.load(str(self.path_stockage))
+        pygame.mixer.music.play(-1)  # -1 pour jouer en boucle
+        time.sleep(temps)
+        self.stop()
+
+    def play_multiple_sounds(sound_files):  # Chatgpt
+        sounds = [
+            Son(i, f"sound_{i}", "test", file) for i, file in enumerate(sound_files)
+        ]
+        for sound in sounds:
+            sound.play()
+            time.sleep(
+                0.1
+            )  # Un léger délai pour éviter de jouer tous les sons en même temps
+
+    def jouer_aleatoire(self):
         pass
