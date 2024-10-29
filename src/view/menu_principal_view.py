b57897214736196854919fd69dd398d66cde60e3
HABIMANA Emmanuel <id2320@DOMENSAI.ECOLE>
2024-10-29 16:58:16 +0100

    SuiteView

--- src/view/menu_principal_view.py
+++ src/view/menu_principal_view.py
@@ -56,3 +56,77 @@ class MenuView(AbstractView):
             print(info)
 
             return MenuView()
+
+
+from InquirerPy import inquirer
+
+from View.abstract_view import AbstractView
+from View.session import Session
+
+from Service.UtilisateurService import UtilisateurService
+
+
+class MenuView(AbstractView):
+    """Vue du menu de l'utilisateur
+
+    Attributes
+    ----------
+    message=''
+        str
+
+    Returns
+    ------
+    view
+        retourne la prochaine vue, celle qui est choisie par l'utilisateur
+    """
+
+    def choisir_menu(self):
+        """Choix du menu suivant de l'utilisateur
+
+        Return
+        ------
+        vue
+            Retourne la vue choisie par l'utilisateur dans le terminal
+        """
+
+        print("\n" + "-" * 50 + "\nMenu Utilisateur\n" + "-" * 50 + "\n")
+
+        choix = inquirer.select(
+            message="Faites votre choix : ",
+            choices=[
+                "Rechercher un son",
+                "Sons",
+                "Playlists",
+                "Infos",
+                "Se déconnecter",
+            ],
+        ).execute()
+
+        match choix:
+            case "Se déconnecter":
+                Session().deconnexion()
+                from View.accueil.accueil_view import AccueilView
+
+                return AccueilView()
+
+            case "Rechercher un son":
+                from View.recherche_son_view import RechSonView
+
+            case "Afficher des pokemons (par appel à un Webservice)":
+                from view.pokemon_vue import PokemonVue
+
+                return PokemonVue()
+
+            case "Infos":
+                MenuView.messageInfoMenu()
+
+    def messageInfoMenu(self):
+        print(
+            "Bonjour et bienvenu sur notre application ! \n"
+            "Vous pouvez vous connecter ou créer un compte pour accéder à nos services. \n"
+            "Cette application a été créée dans le but de vous aider à créer une ambiance pour vos parties de jeux de rôles. \n"
+            "Vous pouvez ajouter des sons à votre playlist et les jouer en boucle ou les arrêter à tout moment afin de permettre une immersion totale des joueurs. \n"
+            "Vous pouvez aussi créer des playlists personnalisées pour chaque partie. \n"
+            "N'hésitez pas à nous contacter pour toute question ou suggestion. \n"
+            "Merci de votre confiance et bonne partie !"
+        )
