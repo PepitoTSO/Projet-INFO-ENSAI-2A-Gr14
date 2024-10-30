842e1622fe9694580911b20db55e7e3beff34cde
HABIMANA Emmanuel <id2320@DOMENSAI.ECOLE>
2024-10-30 17:55:33 +0100

    .

--- src/__main__.py
+++ src/__main__.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     dotenv.load_dotenv(override=True)
 
     # run the Start View
-    current_view = AccueilView()
+    current_view = AccueilView
 
     # while current_view is not none, the application is still running
     while current_view:
@@ -17,9 +17,9 @@ if __name__ == "__main__":
         with open("src/dessin/border.txt", "r", encoding="utf-8") as asset:
             print(asset.read())
         # Display the info of the view
-        current_view.display_info()
+        current_view.display_info(self)
         # ask user for a choice
-        current_view = current_view.make.choice()
+        current_view = current_view.choix
 
     with open("src/dessin/bye.txt", "r", encoding="utf-8") as asset:
         print(asset.read())
