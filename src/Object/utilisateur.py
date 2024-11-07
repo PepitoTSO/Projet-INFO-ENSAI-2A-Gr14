8a1ef9d7e59e3f849e63cf4851fecb5871bc73dc
ArmandVi <armand.vincent77@gmail.com>
2024-11-07 09:25:57 +0100

    Ajout documentation Utilisateur et Utilisateur Service

--- src/Object/utilisateur.py
+++ src/Object/utilisateur.py
@@ -1,5 +1,22 @@
 class Utilisateur:
-    def __init__(self, pseudo: str, mdp_hache: str, list_playlist: list = None):
+    """
+    Permet de créer l'objet métier Utilisateur.
+
+    Attributes
+    ----------
+    pseudo : str
+        Le pseudo de l'utilisateur.
+    mdp_hache : str
+        Le mot de passe haché de l'utilisateur.
+    """
+
+    def __init__(self, pseudo: str, mdp_hache: str):
+
+        if not isinstance(pseudo, str):
+            raise TypeError("Le pseudo n'est pas de type str.")
+        if not isinstance(mdp_hache, str):
+            raise TypeError("Le mdp n'est pas de type str.")
+
         self.pseudo = pseudo
         self.mdp_hache = mdp_hache
 
--- src/Service/UtilisateurService.py
+++ src/Service/UtilisateurService.py
@@ -5,8 +5,31 @@ import hashlib
 
 
 class UtilisateurService:
+    """
+    Permet de gérer les différents services liés à l'utilisateur.
+    """
 
     def se_connecter(self, mdp_nh: str, pseudo_utilisateur: str):
+        """
+        Permet à un utilisateur de se connecter
+
+        Parameters
+        ----------
+        mdp_nh : str
+            Le mot de passe non haché de l'utilisateur.
+        pseudo_utilisateur : str
+            Le pseudo de l'utilisateur.
+
+        Returns
+        -------
+        bool
+            `True` si la connexion est réussie, `False` sinon.
+        """
+
+        if not isinstance(mdp_nh, str):
+            raise TypeError("Le mdp non haché doit être de type str.")
+        if not isinstance(pseudo_utilisateur, str):
+            raise TypeError("Le pseudo doit être de type str.")
         mdp_hache = self.hacher_mot_de_passe(mdp_nh)
         user = Utilisateur(pseudo=pseudo_utilisateur, mdp_hache=mdp_hache)
         reponse = Utilisateur_DAO.se_connecter(user)
@@ -15,23 +38,91 @@ class UtilisateurService:
         return reponse
 
     def hacher_mot_de_passe(self, mdp_nh: str):
+        """
+        Hache le mot de passe avec sel.
+
+        Parameters
+        ----------
+        mdp_nh : str
+            Le mot de passe non haché de l'utilisateur.
+
+        Returns
+        -------
+        str
+            Le mot de passe haché.
+        """
+
+        if not isinstance(mdp_nh, str):
+            raise TypeError("Le mdp non haché doit être de type str.")
         sel = "matae_projet"
         mot_de_passe_sale = sel + mdp_nh
         mdp_hache = hashlib.sha256(mot_de_passe_sale.encode()).hexdigest()
         return mdp_hache
 
     def creer_utilisateur(self, mdp_nh, pseudo_utilisateur):
+        """
+        Permet de créer un nouvel utilisateur.
+
+        Parameters
+        ----------
+        mdp_nh : str
+            Le mot de passe non haché de l'utilisateur.
+        pseudo_utilisateur : str
+            Le pseudo de l'utilisateur.
+
+        Returns
+        -------
+        bool
+            `True` si la création en BDD est réussie, `False` sinon.
+        """
+        if not isinstance(mdp_nh, str):
+            raise TypeError("Le mdp non haché doit être de type str.")
+        if not isinstance(pseudo_utilisateur, str):
+            raise TypeError("Le pseudo doit être de type str.")
+
         mdp_hache = self.hacher_mot_de_passe(mdp_nh=mdp_nh)
         new_user = Utilisateur(mdp_hache=mdp_hache, pseudo=pseudo_utilisateur)
 
         return Utilisateur_DAO.creer_utilisateur(new_user)
 
     def deconnecter_utilisateur(self):
+        """
+        Permet à un utilisateur de se déconnecter
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        Modifie l'utilisateur de l'objet Session
+        """
 
         Session.utilisateur = None
 
     def modifier_utilisateur(self, pseudo_utilisateur: str, nouveau_mdp_nh: str):
-        """Modifie les détails de l'utilisateur courant."""
+        """
+        Permet de modifier un utilisateur. Même si c'est plutôt pour le mdp,
+        On peut aussi modifier le pseudo.
+
+        Parameters
+        ----------
+        pseudo_utilisateur : str
+            Le (nouveau) pseudo de l'utilisateur
+
+        nouveau_mdp : str
+            Le nouveau mot de passe non-haché.
+
+        Returns
+        -------
+        bool
+            `True` si la modification en BDD est réussie, `False` sinon.
+        """
+
+        if not isinstance(nouveau_mdp_nh, str):
+            raise TypeError("Le mdp non haché doit être de type str.")
+        if not isinstance(pseudo_utilisateur, str):
+            raise TypeError("Le pseudo doit être de type str.")
+
         if Session.utilisateur is not None:
             ancien_utilisateur = Utilisateur(
                 pseudo=Session.utilisateur.pseudo,
@@ -42,5 +133,7 @@ class UtilisateurService:
             Session.utilisateur.pseudo = pseudo_utilisateur
 
             user_modif = Utilisateur(pseudo=pseudo_utilisateur, mdp_hache=mdp_hache)
-            reponse = Utilisateur_DAO.modifier_utilisateur(ancien_utilisateur, user_modif)
+            reponse = Utilisateur_DAO.modifier_utilisateur(
+                ancien_utilisateur, user_modif
+            )
         return reponse
