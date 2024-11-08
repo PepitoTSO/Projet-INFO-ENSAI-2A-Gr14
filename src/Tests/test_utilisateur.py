ed2fa95d0e991a26cd10769d67f734ff8b82b510
ArmandVi <armand.vincent77@gmail.com>
2024-11-08 17:00:47 +0100

    Test Utilisateur

--- src/Tests/test_utilisateur.py
+++ src/Tests/test_utilisateur.py
@@ -13,22 +13,23 @@ class TestUtilisateur(unittest.TestCase):
     def test_initialisation_invalide_pseudo(self):
         """Test de la création d'un utilisateur avec un pseudo invalide"""
         with self.assertRaises(TypeError):
-            Utilisateur(12345, "hashed_password")  # Pseudo doit être une chaîne de caractères
+            Utilisateur(12345, "hashed_password")
 
     def test_initialisation_invalide_mdp(self):
         """Test de la création d'un utilisateur avec un mot de passe invalide"""
         with self.assertRaises(TypeError):
-            Utilisateur("johndoe", 12345)  # Mot de passe doit être une chaîne de caractères
+            Utilisateur("johndoe", 12345)
 
     def test_initialisation_invalide_pseudo_et_mdp(self):
         """Test de la création d'un utilisateur avec un pseudo et un mot de passe invalides"""
         with self.assertRaises(TypeError):
-            Utilisateur(12345, 67890)  # Les deux doivent être des chaînes de caractères
+            Utilisateur(12345, 67890)
 
     def test_str(self):
         """Test de la méthode __str__"""
         utilisateur = Utilisateur("johndoe", "hashed_password")
         self.assertEqual(str(utilisateur), "Utilisateur: johndoe")
+
 
 if __name__ == '__main__':
     unittest.main()
