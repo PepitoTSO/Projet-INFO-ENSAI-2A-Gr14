e11c93edfee1240bf65186b4da5fe0551e758b7f
ArmandVi <armand.vincent77@gmail.com>
2024-11-09 10:29:04 +0100

    Ajout pop_db.sql

--- src/utils/reset_database.py
+++ src/utils/reset_database.py
@@ -13,14 +13,14 @@ class ResetDatabase(metaclass=Singleton):
         init_db = open("data/init_db.sql", encoding="utf-8")
         init_db_as_string = init_db.read()
 
-        # pop_db = open("data/pop_db.sql", encoding="utf-8")
-        # pop_db_as_string = pop_db.read()
+        pop_db = open("data/pop_db.sql", encoding="utf-8")
+        pop_db_as_string = pop_db.read()
 
         try:
             with DBConnection().connection as connection:
                 with connection.cursor() as cursor:
                     cursor.execute(init_db_as_string)
-                    # cursor.execute(pop_db_as_string)
+                    cursor.execute(pop_db_as_string)
         except Exception as e:
             print(e)
             raise
