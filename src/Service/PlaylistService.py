35bda875087b781c119b404e8fcbf13a98be86a9
ArmandVi <armand.vincent77@gmail.com>
2024-11-09 10:00:21 +0100

    modif playlist service

--- src/Service/PlaylistService.py
+++ src/Service/PlaylistService.py
@@ -17,53 +17,53 @@ class PlaylistService:
 
     def creer_playlist(self, nom_playlist: str, list_son: list(list()) = [[]]):
 
-        utilisateur = Session.utilisateur
+        utilisateur = Session().utilisateur
 
         playlist = Playlist(utilisateur, None, nom_playlist, list_son)
-        Session.playlist = Playlist_DAO().ajouter_playlist(playlist)
+        Session().playlist = Playlist_DAO().ajouter_playlist(playlist)
 
     def supprimer_playlist(self):
 
-        playlist = Session.playlist
+        playlist = Session().playlist
         Playlist_DAO().supprimer_playlist(playlist)
 
     def modifier_nom_playlist(self, nouveau_nom):
 
-        playlist_a_modif = Session.playlist
-        Session.playlist.nom_playlist = nouveau_nom
-        nouvelle_playlist = Session.playlist
+        playlist_a_modif = Session().playlist
+        Session().playlist.nom_playlist = nouveau_nom
+        nouvelle_playlist = Session().playlist
 
         Playlist_DAO().modifier_nom_playlist(playlist_a_modif, nouvelle_playlist)
 
     def changer_ordre_son(son: Son, ordre: int):
 
-        playlist = Session.playlist
+        playlist = Session().playlist
 
         playlist.changer_ordre(son, ordre)
 
-        Session.playlist = playlist.changer_ordre(son, ordre)
+        Session().playlist = playlist.changer_ordre(son, ordre)
 
         Playlist_DAO().changer_ordre(playlist, son, ordre)
 
     def retirer_son_playlist(son: Son):
 
-        playlist = Session.playlist
+        playlist = Session().playlist
         playlist.retirer_son(son)
-        Session.playlist = playlist
+        Session().playlist = playlist
 
         Playlist_DAO().supprimer_son(playlist, son)
         playlist.retirer_son(son)
-        Session.playlist = playlist
+        Session().playlist = playlist
 
     def copier_playlist(self):
-        playlist = Session.playlist
+        playlist = Session().playlist
         self.creer_playlist(playlist.nom_playlist, playlist.list_son)
 
     def ajouter_son_a_playlist(self, son, ordre):
-        playlist = Session.playlist
+        playlist = Session().playlist
         Playlist_DAO().ajouter_son(playlist, son, ordre)
         playlist.ajouter_son_playlist(son, ordre)
-        Session.playlist = playlist
+        Session().playlist = playlist
 
     def jouer_playlist(self):
 
@@ -81,68 +81,68 @@ class PlaylistService:
                     son_a_jouer.play()
 
     def afficher_playlist(self):
-        utilisateur = Session.utilisateur
+        utilisateur = Session().utilisateur
         liste_playlist = Playlist_DAO().get_all_playlists_by_user(utilisateur)
         # je sais pas quoi faire
 
-    def __init__(self, playlist: Playlist):
-        if not isinstance(playlist, Playlist):
-            raise TypeError("La playlist n'est pas type Playlist.")
+    # def __init__(self, playlist: Playlist):
+    #     if not isinstance(playlist, Playlist):
+    #         raise TypeError("La playlist n'est pas type Playlist.")
 
-        self.playlist = playlist
+    #     self.playlist = playlist
 
-    def creer_playlist(self):
-        Playlist_DAO().creer_playlist(self.playlist.nom_playlist)
+    # def creer_playlist(self):
+    #     Playlist_DAO().creer_playlist(self.playlist.nom_playlist)
 
-    def supprimer_playlist(self):
-        Playlist_DAO().supprimer_playlist(self.playlist.id_playlist)
+    # def supprimer_playlist(self):
+    #     Playlist_DAO().supprimer_playlist(self.playlist.id_playlist)
 
-    def modifier_nom_playlist(self, nouveau_nom: str):
-        if not isinstance(nouveau_nom, str):
-            raise TypeError("Le nom doit être de type str.")
+    # def modifier_nom_playlist(self, nouveau_nom: str):
+    #     if not isinstance(nouveau_nom, str):
+    #         raise TypeError("Le nom doit être de type str.")
 
-        Playlist_DAO().modifier_nom_playlist(self.playlist.id_playlist, nouveau_nom)
+    #     Playlist_DAO().modifier_nom_playlist(self.playlist.id_playlist, nouveau_nom)
 
-    def changer_ordre_son(self, son: Son, ordre: int):
+    # def changer_ordre_son(self, son: Son, ordre: int):
 
-        id_playlist = self.playlist.id_playlist
-        ancien_ordre = Playlist_DAO().get_ordre_son(id_playlist, son)
-        Playlist_DAO().supprimer_son(id_playlist, son)
-        Playlist_DAO().changer_ordre_son(id_playlist, ancien_ordre, False)
-        Playlist_DAO().changer_ordre_son(id_playlist, ordre, True)
-        Playlist_DAO().ajouter_son(id_playlist, son, ordre)
+    #     id_playlist = self.playlist.id_playlist
+    #     ancien_ordre = Playlist_DAO().get_ordre_son(id_playlist, son)
+    #     Playlist_DAO().supprimer_son(id_playlist, son)
+    #     Playlist_DAO().changer_ordre_son(id_playlist, ancien_ordre, False)
+    #     Playlist_DAO().changer_ordre_son(id_playlist, ordre, True)
+    #     Playlist_DAO().ajouter_son(id_playlist, son, ordre)
 
-    def retirer_son_playlist(self, son: Son):
+    # def retirer_son_playlist(self, son: Son):
 
-        id_playlist = self.playlist.id_playlist
-        ancien_ordre = Playlist_DAO().get_ordre_son(id_playlist, son)
-        Playlist_DAO().supprimer_son(id_playlist, son)
-        Playlist_DAO().changer_ordre_son(id_playlist, ancien_ordre, False)
+    #     id_playlist = self.playlist.id_playlist
+    #     ancien_ordre = Playlist_DAO().get_ordre_son(id_playlist, son)
+    #     Playlist_DAO().supprimer_son(id_playlist, son)
+    #     Playlist_DAO().changer_ordre_son(id_playlist, ancien_ordre, False)
 
-    def copier_playlist(self):
+    # def copier_playlist(self):
 
-        id_playlist = self.playlist.id_playlist
-        Playlist_DAO().copier_playlist(id_playlist)
-        # crée une playlist identique
-        # avec id différent et utilisateur différent
+    #     id_playlist = self.playlist.id_playlist
+    #     Playlist_DAO().copier_playlist(id_playlist)
+    #     # crée une playlist identique
+    #     # avec id différent et utilisateur différent
 
-    def ajouter_son_a_playlist(self, son, ordre):
+    # def ajouter_son_a_playlist(self, son, ordre):
 
-        id_playlist = self.playlist.id_playlist
-        Playlist_DAO().changer_ordre_son(id_playlist, ordre, True)
-        Playlist_DAO().ajouter_son(id_playlist, son, ordre)
+    #     id_playlist = self.playlist.id_playlist
+    #     Playlist_DAO().changer_ordre_son(id_playlist, ordre, True)
+    #     Playlist_DAO().ajouter_son(id_playlist, son, ordre)
 
-    def jouer_playlist(self):
+    # def jouer_playlist(self):
 
-        n = len(self.playlist.list_son)
-        api = apifreesound()  # Merlin a modifié ici et après
-        for i in range(n):
-            son = self.playlist.list_son[i][0]
-            api.dl_son(son.id_son)
+    #     n = len(self.playlist.list_son)
+    #     api = apifreesound()  # Merlin a modifié ici et après
+    #     for i in range(n):
+    #         son = self.playlist.list_son[i][0]
+    #         api.dl_son(son.id_son)
 
-        for j in range(n):
-            for i in range(n):
-                if self.playlist.list_son[i][1] == j:
-                    son = self.playlist.list_son[i][0]
-                    son_a_jouer = SonService(son)
-                    son_a_jouer.play()
+    #     for j in range(n):
+    #         for i in range(n):
+    #             if self.playlist.list_son[i][1] == j:
+    #                 son = self.playlist.list_son[i][0]
+    #                 son_a_jouer = SonService(son)
+    #                 son_a_jouer.play()
