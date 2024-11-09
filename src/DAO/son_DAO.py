c0760ed3e1d2186ae2f1d50d2cc6d9300f8a26ea
Tom O'Connor <tom.o-connor@eleve.ensai.fr>
2024-11-09 22:36:57 +0100

    DAO

--- src/DAO/son_DAO.py
+++ src/DAO/son_DAO.py
@@ -1,9 +1,11 @@
 from utils.singleton import Singleton
-from db_connection import DBConnection
+from DAO.db_connection import DBConnection
 from Object.son import Son
 from typing import List
 import psycopg2
 import psycopg2.extras
+import logging
+from utils.log_decorator import log
 
 
 class Son_DAO(metaclass=Singleton):
@@ -12,19 +14,19 @@ class Son_DAO(metaclass=Singleton):
     Uses the Singleton pattern to ensure a single instance.
     """
 
+    @log
     def ajouter_son(self, son: Son) -> bool:
         """
         Adds a new 'son' to the database.
         """
         # Convert tags to a comma-separated string if it is a list
-        tags_string = ",".join(son.tags) if isinstance(son.tags, list) else son.tags
+        tags_string = ", ".join(son.tags) if isinstance(son.tags, list) else son.tags
 
         try:
             with DBConnection().connection as connection:
                 with connection.cursor(
                     cursor_factory=psycopg2.extras.RealDictCursor
                 ) as cursor:
-                    # Execute the insert query using %s placeholders
                     cursor.execute(
                         """
                         INSERT INTO bdd.son (nom_son, tags, path_stockage)
@@ -37,23 +39,16 @@ class Son_DAO(metaclass=Singleton):
                             str(son.path_stockage),
                         ),
                     )
-                    # Fetch the generated id_son
                     res = cursor.fetchone()
-                    print(res)
 
-                    # Check if the result is valid
                     if res is not None:
-                        print(
-                            f"Son added with id: {res['id_son']}"
-                        )  # Access the result by column name
+                        print(f"Son added with id: {res['id_son']}")
                         return True
 
         except Exception as e:
-            # Print a detailed error message including the type and the string representation of the error
             print(f"Error adding son: {e.__class__.__name__}: {e}")
             return False
 
-        # Return False if the insertion failed without triggering an exception
         return False
 
     def get_son_by_id(self, id_son: int) -> Son:
@@ -70,7 +65,11 @@ class Son_DAO(metaclass=Singleton):
             son = Son(
                 id_son=res["id_son"],
                 nom=res["nom_son"],
-                tags=[res["tags"]],
+                tags=(
+                    res["tags"].split(",")
+                    if isinstance(res["tags"], str)
+                    else res["tags"]
+                ),
                 path_stockage=res["path_stockage"],
             )
             return son
@@ -90,7 +89,11 @@ class Son_DAO(metaclass=Singleton):
             son = Son(
                 id_son=res["id_son"],
                 nom=res["nom_son"],
-                tags=[res["tags"]],
+                tags=(
+                    res["tags"].split(",")
+                    if isinstance(res["tags"], str)
+                    else res["tags"]
+                ),
                 path_stockage=res["path_stockage"],
             )
             return son
@@ -100,30 +103,29 @@ class Son_DAO(metaclass=Singleton):
         """
         Récupère tous les sons de la base de données.
         """
+        list_son = []
         try:
-            list_son = []
             with DBConnection().connection as connection:
                 with connection.cursor(
                     cursor_factory=psycopg2.extras.RealDictCursor
                 ) as cursor:
                     cursor.execute("SELECT * FROM bdd.son;")
                     res = cursor.fetchall()
-            for son_data in res:
 
-                print(
-                    son_data["id_son"],
-                    son_data["nom_son"],
-                    [son_data["tags"]],
-                    son_data["path_stockage"],
-                )
+            for son_data in res:
                 son = Son(
                     id_son=son_data["id_son"],
                     nom=son_data["nom_son"],
-                    tags=[son_data["tags"]],
+                    tags=(
+                        son_data["tags"].split(",")
+                        if isinstance(son_data["tags"], str)
+                        else son_data["tags"]
+                    ),
                     path_stockage=son_data["path_stockage"],
                 )
                 list_son.append(son)
             return list_son
+
         except Exception as e:
             print(f"Error get_all_son :{e}")
             return None
@@ -146,27 +148,36 @@ class Son_DAO(metaclass=Singleton):
         Retrieves all 'sons' from the specified playlist along with their order in the playlist.
         """
         sons = []
-        with DBConnection().connection as connection:
-            with connection.cursor(
-                cursor_factory=psycopg2.extras.RealDictCursor
-            ) as cursor:
-                cursor.execute(
-                    """
-                    SELECT s.id_son, s.nom_son, s.tags, s.path_stockage, psj.ordre_son_playlist
-                    FROM bdd.son s
-                    JOIN bdd.playlist_son_join psj ON s.id_son = psj.id_son
-                    WHERE psj.id_playlist = %s
-                    """,
-                    (id_playlist,),
-                )
-                res = cursor.fetchall()
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor(
+                    cursor_factory=psycopg2.extras.RealDictCursor
+                ) as cursor:
+                    cursor.execute(
+                        """
+                        SELECT s.id_son, s.nom_son, s.tags, s.path_stockage, psj.ordre_son_playlist
+                        FROM bdd.son s
+                        JOIN bdd.playlist_son_join psj ON s.id_son = psj.id_son
+                        WHERE psj.id_playlist = %s
+                        """,
+                        (id_playlist,),
+                    )
+                    res = cursor.fetchall()
 
-        for son_data in res:
-            son = Son(
-                id_son=son_data["id_son"],
-                nom=son_data["nom_son"],
-                tags=son_data["tags"],
-                path_stockage=son_data["path_stockage"],
-            )
-            sons.append([son, son_data["ordre_son_playlist"]])
-        return sons
+            for son_data in res:
+                son = Son(
+                    id_son=son_data["id_son"],
+                    nom=son_data["nom_son"],
+                    tags=(
+                        son_data["tags"].split(",")
+                        if isinstance(son_data["tags"], str)
+                        else son_data["tags"]
+                    ),
+                    path_stockage=son_data["path_stockage"],
+                )
+                sons.append([son, son_data["ordre_son_playlist"]])
+            return sons
+
+        except Exception as e:
+            print(f"Error get_all_son_ordre_by_id_playlist :{e}")
+            return []
--- src/DAO/tDAOplaylist.py
+++ src/DAO/tDAOplaylist.py
@@ -10,7 +10,7 @@ son1 = Son(
 )
 son2 = Son(id_son=2, nom="son2", tags=["tags"], path_stockage="data/test.mp3")
 son3 = Son(id_son=3, nom="son3", tags=["pluie"], path_stockage="data/test.mp3")
-utilisateur = Utilisateur("a", "b")
+utilisateur = Utilisateur("user1", "b")
 playlist = Playlist(
     utilisateur=utilisateur,
     id_playlist=1,
@@ -22,12 +22,16 @@ playlistDAO = Playlist_DAO()
 utilisateurDAO = Utilisateur_DAO()
 sonDAO = Son_DAO()
 
+# playlistDAO.ajouter_playlist(playlist)
 # utilisateurDAO.creer_utilisateur(utilisateur)
 # sonDAO.ajouter_son(son1)
 # sonDAO.ajouter_son(son2)
-# print(sonDAO.get_all_son()[0])
-# sonDAO.get_all_son_ordre_by_id_playlist() - pas encore tester !!!!
-# print(sonDAO.get_son_by_id(2))
-# print(sonDAO.get_son_by_name("son1"))
+# print(sonDAO.get_all_son()[12])
+# sonDAO.supprimer_son(13)
+# for k in range(0, 3):
+#    son = sonDAO.get_all_son_ordre_by_id_playlist(1)[k][0]
+#    print(son)
+# print(sonDAO.get_son_by_id(13))
+# print(sonDAO.get_son_by_name("Song 1"))
 # playlistDAO.ajouter_playlist(playlist)
-print(playlistDAO.get_sons_by_playlist(playlist))
+# print(playlistDAO.get_sons_by_playlist(playlist))
